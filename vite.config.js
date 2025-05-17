620289528767e387bca3e3075d59c9b5744a1f4c
rostick <rostick@offerwell.com>
2025-05-18 00:51:06 +0300

    Small fix

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,16 @@
-// vite.config.js
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig(({ command }) => ({
-  root: 'src',                    // where your index.html lives
-  publicDir: '../static',         // your “public” folder
-  base: command === 'serve'
-    ? '/'                          // in dev: load from “/”
-    : '/solar-system/',            // in prod: load from “/solar-system/”
+  root: 'src',
+  publicDir: '../static',
+  base: command === 'serve' ? '/' : '/',     // always load assets from "/"
   server: {
     host: true,
     open: false,
   },
   build: {
-    outDir: '../dist',            // where to drop the final build
+    outDir: '../dist',
     emptyOutDir: true,
     sourcemap: true,
-    rollupOptions: {
-      input: resolve(__dirname, 'src/index.html'),
-    },
   },
 }));
